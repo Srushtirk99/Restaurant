1f6b7b26ac8b04815fcfb1570d1392cbefcb7bf7
Srushtirk99 <srushtik1310@gmail.com>
2025-03-08 21:55:46 +0530

    Add files via upload

---
+++ backend/controllers/booktable.js
@@ -0,0 +1,13 @@
+import { insertBooking } from "../models/BookTableModel.js";
+
+// create Booking
+export const createBooking = (req, res) => {
+  const data = req.body;
+  insertBooking(data, (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(results);
+    }
+  });
+};
